614eece8cf9779e2bfa986f042c9260bdbc71787
DarkKnight1990 <pikludey1990@gmail.com>
2021-09-10 16:35:17 +0530

    service provider model

--- backend/providers/models.py
+++ backend/providers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ServiceProvider(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    language = models.CharField(max_length=10)
+    currency = models.CharField(max_length=3)
+
+    def __str__(self) -> str:
+        return self.name
