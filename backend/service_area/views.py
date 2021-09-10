06549c23d9274b0faf52bb00817544cc1bb0cf49
DarkKnight1990 <pikludey1990@gmail.com>
2021-09-11 01:25:20 +0530

    search area api view added

--- backend/service_area/views.py
+++ backend/service_area/views.py
@@ -1,9 +1,19 @@
-from rest_framework import viewsets
+from django.contrib.gis.geos import Point
+
+from rest_framework import views, viewsets
+from rest_framework.response import Response
 
 from service_area.models import ServiceArea
-from service_area.serializers import ServiceAreaSerializer
+from service_area.serializers import SearchServiceAreaSerializer, ServiceAreaSerializer
 
 
 class ServiceAreaViewset(viewsets.ModelViewSet):
     serializer_class = ServiceAreaSerializer
     queryset = ServiceArea.objects.all()
+
+
+class SearchServiceAreaView(views.APIView):
+    def get(self, request, longitude, latitude):
+        service_areas = ServiceArea.objects.filter(service_area__contains=Point(float(longitude), float(latitude)))
+        serializer = SearchServiceAreaSerializer(service_areas, many=True)
+        return Response(serializer.data)
