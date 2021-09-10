ce01a48e131ee0480ff29dc4a299f708674cae82
DarkKnight1990 <pikludey1990@gmail.com>
2021-09-11 01:23:13 +0530

    updated factory data and inherit model to DjangoModelFactory

--- backend/service_area/factories.py
+++ backend/service_area/factories.py
@@ -10,24 +10,28 @@ from service_area.models import ServiceArea
 
 fake = Factory.create()
 
+"""
+Generated this API response from:
+https://geojson.io/#map=2/20.0/0.0
+"""
+
 
 def test_area_json():
     return {
         "type": "Polygon",
         "coordinates": [
             [
-                [77.57669448852539, 12.985656799993347],
-                [77.57360458374022, 12.974616644461282],
-                [77.58235931396484, 12.968929100296371],
-                [77.60433197021484, 12.970434639348912],
-                [77.60244369506836, 12.985991342505251],
-                [77.57669448852539, 12.985656799993347],
+                [71.89453125, 25.64152637306577],
+                [73.125, 18.93746442964186],
+                [84.1552734375, 18.895892559415024],
+                [87.36328125, 25.403584973186703],
+                [71.89453125, 25.64152637306577],
             ]
         ],
     }
 
 
-class ServiceAreaFactory(factory.Factory):
+class ServiceAreaFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = ServiceArea
 
