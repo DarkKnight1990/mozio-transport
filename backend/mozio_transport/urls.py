8d75df30b8cbce3de3be6decac0100616466d382
DarkKnight1990 <pikludey1990@gmail.com>
2021-09-10 20:51:32 +0530

    add service area url

--- backend/mozio_transport/urls.py
+++ backend/mozio_transport/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("provider/", include("provider.urls")),
+    path("service-area/", include("service_area.urls")),
 ]
