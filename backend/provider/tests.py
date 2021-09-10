80450b68f1ca0387dc93d47a3e85471e9736f39b
DarkKnight1990 <pikludey1990@gmail.com>
2021-09-10 17:46:20 +0530

    test case to check service provider creation

--- backend/provider/tests.py
+++ backend/provider/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import test, status
+
+from provider.models import ServiceProvider
+from provider.factories import ServiceProviderFactory
+
+
+class ServiceProviderTests(test.APITestCase):
+    def test_create_service_provider(self):
+        """
+        test if our API creates the service provider
+        """
+        url = reverse("service-provider-list")
+        provider = ServiceProviderFactory.build()
+        payload = {
+            "name": provider.name,
+            "email": provider.email,
+            "phone_number": provider.phone_number,
+            "language": provider.language,
+            "currency": provider.currency,
+        }
+        response = self.client.post(url, payload, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ServiceProvider.objects.count(), 1)
+        self.assertEqual(ServiceProvider.objects.get().name, provider.name)
