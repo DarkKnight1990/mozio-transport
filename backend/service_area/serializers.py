2c0e86cb3fe13e6856f1d946cbd89c2d8d923533
DarkKnight1990 <pikludey1990@gmail.com>
2021-09-11 01:23:45 +0530

    added SearchServiceArea serializer

--- backend/service_area/serializers.py
+++ backend/service_area/serializers.py
@@ -5,8 +5,24 @@ from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from service_area.models import ServiceArea
 
 
+"""
+Using the rest_framework.gis module:
+followed this link:
+
+https://github.com/openwisp/django-rest-framework-gis
+"""
+
+
 class ServiceAreaSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = ServiceArea
         fields = ["id", "name", "provider", "service_area", "price"]
         geo_field = "service_area"
+
+
+class SearchServiceAreaSerializer(serializers.ModelSerializer):
+    provider = serializers.StringRelatedField()
+
+    class Meta:
+        model = ServiceArea
+        fields = ("name", "provider", "price")
