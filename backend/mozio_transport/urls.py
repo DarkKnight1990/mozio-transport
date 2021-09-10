69406f8832903ebb0597cf120ab3cbff79cf1200
DarkKnight1990 <pikludey1990@gmail.com>
2021-09-11 01:22:03 +0530

    update url name

--- backend/mozio_transport/urls.py
+++ backend/mozio_transport/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("provider/", include("provider.urls")),
-    path("service-area/", include("service_area.urls")),
+    path("area-api/", include("service_area.urls")),
 ]
